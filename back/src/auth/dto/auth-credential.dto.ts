1510ad796ca0689f70246a2bb1842b3ef362f5eb
InKyoJeong <viapolar@naver.com>
2023-03-21 14:59:15 +0900

    feat(back): add validation pipe to signup

--- back/src/auth/dto/auth-credential.dto.ts
+++ back/src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: '비밀번호는 영어 또는 숫자 조합으로 입력해주세요.',
+  })
   password: string;
 }
