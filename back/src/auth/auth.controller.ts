9b94737f7e0393e4b7ab474e9dced593b675f5b5
InKyoJeong <viapolar@naver.com>
2023-03-21 23:19:59 +0900

    feat(back): add AuthGuard to feed

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,16 +1,6 @@
-import {
-  Body,
-  Controller,
-  Post,
-  Req,
-  UseGuards,
-  ValidationPipe,
-} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { GetUser } from './decorator/get-user.decorator';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
-import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -28,11 +18,5 @@ export class AuthController {
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signin(authCredentialsDto);
-  }
-
-  @Post('/test')
-  @UseGuards(AuthGuard())
-  test(@GetUser() user: User) {
-    console.log(user);
   }
 }
--- back/src/feed/feed.module.ts
+++ back/src/feed/feed.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { FeedController } from './feed.controller';
 import { Feed } from './feed.entity';
 import { FeedService } from './feed.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Feed])],
+  imports: [TypeOrmModule.forFeature([Feed]), AuthModule],
   controllers: [FeedController],
   providers: [FeedService],
 })
