955c990d0044e549c0da430d2ceec9127322971b
InKyoJeong <viapolar@naver.com>
2023-03-30 03:47:26 +0900

    feat(front): add signup form validate

--- mylog/src/screens/auth/SignupScreen.tsx
+++ mylog/src/screens/auth/SignupScreen.tsx
@@ -1,14 +1,79 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
 
 import CustomButton from '@/components/CustomButton';
+import useForm from '@/hooks/common/useForm';
+import InputField from '@/components/InputField';
+import {validateSignup} from '@/utils/validate';
 
 function SignupScreen() {
+  const signup = useForm({
+    initialValue: {username: '', password: '', passwordConfirm: ''},
+    validate: validateSignup,
+  });
+
+  const passwordRef = useRef<TextInput | null>(null);
+  const passwordConfirmRef = useRef<TextInput | null>(null);
+
+  const handleSubmit = () => {};
+
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <CustomButton label="회원가입" variant="filled" size="large" />
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.inputContainer}>
+          <InputField
+            {...signup.getTextInputProps('username')}
+            error={signup.errors.username}
+            touched={signup.touched.username}
+            placeholder="아이디"
+            maxLength={20}
+            inputMode="email"
+            clearButtonMode="always"
+            autoCapitalize="none"
+            returnKeyType="next"
+            blurOnSubmit={false}
+            onSubmitEditing={() => {
+              passwordRef.current?.focus();
+            }}
+          />
+          <InputField
+            {...signup.getTextInputProps('password')}
+            error={signup.errors.password}
+            touched={signup.touched.password}
+            ref={passwordRef}
+            placeholder="비밀번호"
+            maxLength={20}
+            clearButtonMode="always"
+            secureTextEntry
+            onSubmitEditing={() => {
+              passwordConfirmRef.current?.focus();
+            }}
+          />
+          <InputField
+            {...signup.getTextInputProps('passwordConfirm')}
+            error={signup.errors.passwordConfirm}
+            touched={signup.touched.passwordConfirm}
+            ref={passwordConfirmRef}
+            placeholder="비밀번호 확인"
+            maxLength={20}
+            clearButtonMode="always"
+            secureTextEntry
+            onSubmitEditing={handleSubmit}
+          />
+        </View>
+        <CustomButton
+          label="회원가입"
+          variant="filled"
+          size="large"
+          isValid={!signup.hasErrors}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -16,10 +81,12 @@ function SignupScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     marginHorizontal: 30,
     marginVertical: 30,
+  },
+  inputContainer: {
+    gap: 30,
+    marginBottom: 40,
   },
 });
 
--- mylog/src/utils/validate.ts
+++ mylog/src/utils/validate.ts
@@ -1,10 +1,42 @@
+import {getObjectWithValue} from '.';
 import {errorMessages} from '@/constants/messages';
 import {numbers} from '@/constants/numbers';
-import {getObjectWithValue} from '.';
 
 type LoginInfo = {
   username: string;
   password: string;
+};
+
+type SignupInfo = {
+  username: string;
+  password: string;
+  passwordConfirm: string;
+};
+
+const isValidUsernameFormat = (username: string) => {
+  return /^[a-zA-Z0-9]*$/.test(username);
+};
+
+const isValidPasswordFormat = (password: string) => {
+  return /^[a-zA-Z0-9`'~!@#$%^&*()-_=+]*$/.test(password);
+};
+
+const isValidUsernameLength = (username: string) => {
+  return (
+    username.length >= numbers.MIN_USERNAME_LENGTH &&
+    username.length <= numbers.MAX_USERNAME_LENGTH
+  );
+};
+
+const isValidPasswordLength = (password: string) => {
+  return (
+    password.length >= numbers.MIN_PASSWORD_LENGTH &&
+    password.length <= numbers.MAX_PASSWORD_LENGTH
+  );
+};
+
+const isMatchPasswordConfirm = (password: string, passwordConfirm: string) => {
+  return password === passwordConfirm;
 };
 
 const validateLogin = (values: LoginInfo) => {
@@ -14,27 +46,53 @@ const validateLogin = (values: LoginInfo) => {
   );
   const {username, password} = values;
 
-  if (!/^[a-zA-Z0-9]*$/.test(username)) {
+  if (!isValidUsernameFormat(username)) {
     errors.username = errorMessages.INVALID_USERNAME_FORMAT;
   }
-  if (
-    username.length < numbers.MIN_USERNAME_LENGTH ||
-    username.length > numbers.MAX_USERNAME_LENGTH
-  ) {
+  if (!isValidUsernameLength(username)) {
     errors.username = errorMessages.INVALID_USERNAME_LENGTH;
   }
-
-  if (!/^[a-zA-Z0-9`'~!@#$%^&*()-_=+]*$/.test(password)) {
+  if (!isValidPasswordFormat(password)) {
     errors.password = errorMessages.INVALID_PASSWORD_FORMAT;
   }
-  if (
-    password.length < numbers.MIN_PASSWORD_LENGTH ||
-    password.length > numbers.MAX_PASSWORD_LENGTH
-  ) {
+  if (!isValidPasswordLength(password)) {
     errors.password = errorMessages.INVALID_PASSWORD_LENGTH;
   }
 
   return errors;
 };
 
-export {validateLogin};
+const validateSignup = (values: SignupInfo) => {
+  const errors = getObjectWithValue(
+    Object.keys(values) as (keyof SignupInfo)[],
+    '',
+  );
+  const {username, password, passwordConfirm} = values;
+
+  if (!isValidUsernameFormat(username)) {
+    errors.username = errorMessages.INVALID_USERNAME_FORMAT;
+  }
+  if (!isValidUsernameLength(username)) {
+    errors.username = errorMessages.INVALID_USERNAME_LENGTH;
+  }
+  if (!isValidPasswordFormat(password)) {
+    errors.password = errorMessages.INVALID_PASSWORD_FORMAT;
+  }
+  if (!isValidPasswordLength(password)) {
+    errors.password = errorMessages.INVALID_PASSWORD_LENGTH;
+  }
+
+  if (!isValidPasswordFormat(passwordConfirm)) {
+    errors.passwordConfirm = errorMessages.INVALID_PASSWORD_FORMAT;
+  }
+  if (!isValidPasswordLength(passwordConfirm)) {
+    errors.passwordConfirm = errorMessages.INVALID_PASSWORD_LENGTH;
+  }
+  if (!isMatchPasswordConfirm(password, passwordConfirm)) {
+    errors.passwordConfirm = errorMessages.NOT_MATCH_PASSWORD;
+  }
+
+  return errors;
+};
+
+export {validateLogin, validateSignup};
