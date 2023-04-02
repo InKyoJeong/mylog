29f5cec205da3b48785d232f378f7fc3192a0d3b
InKyoJeong <viapolar@naver.com>
2023-04-02 19:45:22 +0900

    feat(front): add keyboard persist view - 인풋창 내부를 클릭하면 키보드가 한번 내려가는 동작 방지

--- mylog/src/screens/auth/SignupScreen.tsx
+++ mylog/src/screens/auth/SignupScreen.tsx
@@ -1,15 +1,11 @@
 import React, {useRef} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  TextInput,
-  View,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, TextInput, View} from 'react-native';
 
 import CustomButton from '@/components/CustomButton';
-import useForm from '@/hooks/common/useForm';
 import InputField from '@/components/InputField';
+import KeyboardPersistView from '@/components/KeyboardPersistView';
+import CustomKeyboardAvoidingView from '@/components/CustomKeyboardAvoidingView';
+import useForm from '@/hooks/common/useForm';
 import {validateSignup} from '@/utils/validate';
 
 function SignupScreen() {
@@ -25,56 +21,57 @@ function SignupScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.inputContainer}>
-          <InputField
-            autoFocus
-            {...signup.getTextInputProps('username')}
-            error={signup.errors.username}
-            touched={signup.touched.username}
-            placeholder="아이디"
-            maxLength={20}
-            inputMode="email"
-            clearButtonMode="always"
-            autoCapitalize="none"
-            returnKeyType="next"
-            blurOnSubmit={false}
-            onSubmitEditing={() => {
-              passwordRef.current?.focus();
-            }}
+      <KeyboardPersistView>
+        <CustomKeyboardAvoidingView>
+          <View style={styles.inputContainer}>
+            <InputField
+              {...signup.getTextInputProps('username')}
+              error={signup.errors.username}
+              touched={signup.touched.username}
+              placeholder="아이디"
+              maxLength={20}
+              autoFocus
+              inputMode="email"
+              returnKeyType="next"
+              blurOnSubmit={false}
+              onSubmitEditing={() => {
+                passwordRef.current?.focus();
+              }}
+            />
+            <InputField
+              {...signup.getTextInputProps('password')}
+              error={signup.errors.password}
+              touched={signup.touched.password}
+              ref={passwordRef}
+              placeholder="비밀번호"
+              textContentType="newPassword"
+              maxLength={20}
+              secureTextEntry
+              returnKeyType="next"
+              onSubmitEditing={() => {
+                passwordConfirmRef.current?.focus();
+              }}
+            />
+            <InputField
+              {...signup.getTextInputProps('passwordConfirm')}
+              error={signup.errors.passwordConfirm}
+              touched={signup.touched.passwordConfirm}
+              ref={passwordConfirmRef}
+              placeholder="비밀번호 확인"
+              maxLength={20}
+              secureTextEntry
+              returnKeyType="join"
+              onSubmitEditing={handleSubmit}
+            />
+          </View>
+          <CustomButton
+            label="회원가입"
+            variant="filled"
+            size="large"
+            isValid={!signup.hasErrors}
           />
-          <InputField
-            {...signup.getTextInputProps('password')}
-            error={signup.errors.password}
-            touched={signup.touched.password}
-            ref={passwordRef}
-            placeholder="비밀번호"
-            maxLength={20}
-            clearButtonMode="always"
-            secureTextEntry
-            onSubmitEditing={() => {
-              passwordConfirmRef.current?.focus();
-            }}
-          />
-          <InputField
-            {...signup.getTextInputProps('passwordConfirm')}
-            error={signup.errors.passwordConfirm}
-            touched={signup.touched.passwordConfirm}
-            ref={passwordConfirmRef}
-            placeholder="비밀번호 확인"
-            maxLength={20}
-            clearButtonMode="always"
-            secureTextEntry
-            onSubmitEditing={handleSubmit}
-          />
-        </View>
-        <CustomButton
-          label="회원가입"
-          variant="filled"
-          size="large"
-          isValid={!signup.hasErrors}
-        />
-      </ScrollView>
+        </CustomKeyboardAvoidingView>
+      </KeyboardPersistView>
     </SafeAreaView>
   );
 }
