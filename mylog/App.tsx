31dde0358522c8dc2eddf2617ecd287ac5451f6e
InKyoJeong <viapolar@naver.com>
2023-04-05 14:46:38 +0900

    chore(front): install react-query & axios

--- mylog/App.tsx
+++ mylog/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 import RootNavigator from '@/navigations/root/RootNavigator';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {},
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <StatusBar barStyle={'dark-content'} />
       <NavigationContainer>
         <RootNavigator />
       </NavigationContainer>
-    </>
+    </QueryClientProvider>
   );
 }
 
