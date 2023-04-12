dd6e3e31836d5f58c64014773a1d04cdf19cc270
InKyoJeong <viapolar@naver.com>
2023-04-12 21:32:34 +0900

    front(chore): install react-query devtools

--- mylog/App.tsx
+++ mylog/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {QueryClientProvider} from '@tanstack/react-query';
 
 import RootNavigator from '@/navigations/root/RootNavigator';
+import queryClient from '@/api/queryClient';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {},
-  },
-});
+if (__DEV__) {
+  import('react-query-native-devtools').then(({addPlugin}) => {
+    addPlugin({queryClient});
+  });
+}
 
 function App() {
   return (
