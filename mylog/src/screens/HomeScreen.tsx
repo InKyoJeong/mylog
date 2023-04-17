f168b781d309773ebcf4317bb93fb54c674b4aea
InKyoJeong <viapolar@naver.com>
2023-04-18 00:35:41 +0900

    feat(front): select location with longPress

--- mylog/src/screens/HomeScreen.tsx
+++ mylog/src/screens/HomeScreen.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, StyleSheet, View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {
+  LatLng,
+  LongPressEvent,
+  Marker,
+  PROVIDER_GOOGLE,
+} from 'react-native-maps';
 import type {StackScreenProps} from '@react-navigation/stack';
 import type {DrawerScreenProps} from '@react-navigation/drawer';
 import type {CompositeScreenProps} from '@react-navigation/native';
@@ -10,17 +15,36 @@ import type {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator
 import {homeNavigations, mainNavigations} from '@/constants/navigations';
 import useAuth from '@/hooks/queries/useAuth';
 import getMapStyle from '@/style/mapStyle';
+import {colors} from '@/constants/colors';
 
 type HomeScreenProps = CompositeScreenProps<
   StackScreenProps<HomeStackParamList, typeof homeNavigations.MAP_HOME>,
   DrawerScreenProps<MainDrawerParamList, typeof mainNavigations.HOME>
 >;
 
+const markers: (LatLng & {id: number})[] = [
+  {
+    id: 1,
+    latitude: 37.3814,
+    longitude: 127.1291,
+  },
+  {
+    id: 2,
+    latitude: 37.3814,
+    longitude: 127.1091,
+  },
+];
+
 function HomeScreen({navigation}: HomeScreenProps) {
   const {logoutMutate} = useAuth();
+  const [selectedLocation, setSelectedLocation] = useState<LatLng>();
 
   const handleLogout = () => {
     logoutMutate.mutate(null);
+  };
+
+  const handleSelectLocation = ({nativeEvent}: LongPressEvent) => {
+    setSelectedLocation(nativeEvent.coordinate);
   };
 
   return (
@@ -33,16 +57,32 @@ function HomeScreen({navigation}: HomeScreenProps) {
         followsUserLocation={true}
         customMapStyle={getMapStyle('light')}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: 37.3814,
+          longitude: 127.1191,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+        onLongPress={handleSelectLocation}>
+        {markers.map(marker => (
+          <Marker
+            key={marker.id}
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }}>
+            <View style={styles.marker} />
+          </Marker>
+        ))}
+        {selectedLocation && (
+          <Marker coordinate={selectedLocation}>
+            <View style={styles.selectedMarker} />
+          </Marker>
+        )}
+      </MapView>
       <View style={styles.buttons}>
         <Button title="로그아웃" onPress={handleLogout} />
         <Button
-          title="AddLocation"
+          title="위치 추가"
           onPress={() => navigation.navigate(homeNavigations.ADD_LOCATION)}
         />
       </View>
@@ -54,10 +94,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  marker: {
+    backgroundColor: colors.PINK_600,
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderBottomRightRadius: 3,
+    borderColor: colors.GRAY_500,
+    borderWidth: 1.5,
+  },
+  selectedMarker: {
+    backgroundColor: colors.WHITE,
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderBottomRightRadius: 3,
+    borderColor: colors.GRAY_500,
+    borderWidth: 1.5,
+  },
   buttons: {
     position: 'absolute',
     bottom: 40,
-    backgroundColor: 'red',
+    backgroundColor: colors.PINK_600,
   },
 });
 
