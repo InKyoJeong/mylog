59fcd2a46b0a8d5174ec91648b3720601532d997
InKyoJeong <viapolar@naver.com>
2023-04-20 21:17:20 +0900

    fix(front): change drawer profile position

--- mylog/src/navigations/drawer/CustomDrawerContent.tsx
+++ mylog/src/navigations/drawer/CustomDrawerContent.tsx
@@ -56,6 +56,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.WHITE,
   },
   userInfoContainer: {
+    alignItems: 'center',
     marginVertical: 30,
     marginHorizontal: 15,
   },
