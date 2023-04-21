22ca67d3a39612f2e915793c53ad89c03a1fef13
InKyoJeong <viapolar@naver.com>
2023-04-22 02:45:49 +0900

    feat(front): add queryclient stale time

--- mylog/src/api/queryClient.ts
+++ mylog/src/api/queryClient.ts
@@ -3,6 +3,10 @@ import {QueryClient} from '@tanstack/react-query';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      suspense: true,
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      staleTime: 60000,
       retry: false,
     },
   },
