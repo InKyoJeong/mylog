521e68f7d183acc686e276c57bcb7a7e6bf53740
InKyoJeong <viapolar@naver.com>
2023-04-28 19:22:19 +0900

    fix: fix modal and LayoutAnimation conflicting issue - 라이브러리 수정하여 설치

--- mylog/src/api/index.ts
+++ mylog/src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3030';
+// const BASE_URL = 'http://localhost:3030';
 // const BASE_URL = 'http://10.0.2.2:3030'; //android emulator
-// const BASE_URL = 'http://192.168.0.55:3030'; //ios device
+const BASE_URL = 'http://192.168.0.55:3030'; //ios device
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
