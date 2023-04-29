1dd92b2e9e758e648eb88b571641e77e8107571e
InKyoJeong <viapolar@naver.com>
2023-04-30 02:03:46 +0900

    refactor(front): remove duplicated date code

--- mylog/src/utils/date.ts
+++ mylog/src/utils/date.ts
@@ -1,18 +1,26 @@
 import {months} from '@/constants/numbers';
 import type {Locale} from '@/types';
 
-function getDateWithSeparator(date: Date, separator: string = '') {
-  const year = String(date.getFullYear());
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-
-  return [year, month, day].join(separator);
-}
-
-function getDateLocaleFormat(date: Date, locale: Locale = 'ko') {
+function getDateDetails(date: Date) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
+
+  return {year, month, day};
+}
+
+function getDateWithSeparator(date: Date, separator: string = '') {
+  const {year, month, day} = getDateDetails(date);
+
+  return [
+    String(year),
+    String(month).padStart(2, '0'),
+    String(day).padStart(2, '0'),
+  ].join(separator);
+}
+
+function getDateLocaleFormat(date: Date, locale: Locale = 'ko') {
+  const {year, month, day} = getDateDetails(date);
 
   switch (locale) {
     case 'ko': {
