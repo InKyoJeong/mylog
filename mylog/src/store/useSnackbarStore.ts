a15c705c430b519b582a4406a62f43ed8b56c1f4
InKyoJeong <viapolar@naver.com>
2023-05-04 23:32:33 +0900

    feat(front): add snackbar store

---
+++ mylog/src/store/useSnackbarStore.ts
@@ -0,0 +1,23 @@
+import {create} from 'zustand';
+
+type SnackbarType = 'success' | 'fail';
+
+interface SnackbarState {
+  isVisible: boolean;
+  info: {message: string; type?: 'success' | 'fail'};
+  show: (message: string, type?: SnackbarType) => void;
+  hide: () => void;
+}
+
+const useSnackbarStore = create<SnackbarState>((set, get) => ({
+  isVisible: false,
+  info: {message: '', type: 'success'},
+  show: (message: string, type: SnackbarType = 'success') => {
+    set({isVisible: true, info: {message, type}});
+  },
+  hide: () => {
+    set({isVisible: false, info: {message: '', type: get().info.type}});
+  },
+}));
+
+export default useSnackbarStore;
