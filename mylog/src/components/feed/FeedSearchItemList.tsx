294aacdeeff323c268c5e2f175cb7dd5007b8dff
InKyoJeong <viapolar@naver.com>
2023-05-17 21:12:39 +0900

    refactor(front): remove inline styles

--- mylog/src/components/feed/FeedSearchItemList.tsx
+++ mylog/src/components/feed/FeedSearchItemList.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {Keyboard, FlatList, Pressable, StyleSheet, View} from 'react-native';
-import Animated, {SlideInRight} from 'react-native-reanimated';
+import Animated, {FadeInRight} from 'react-native-reanimated';
 import {useNavigation} from '@react-navigation/native';
 import type {DrawerNavigationProp} from '@react-navigation/drawer';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -56,33 +56,21 @@ function FeedSearchItemList({}: FeedSearchItemListProps) {
         indicatorStyle="black"
         ListEmptyComponent={<InfoMessage message="검색 결과가 없습니다." />}
         ListHeaderComponent={
-          <View
-            style={{
-              flexDirection: 'row',
-              gap: 5,
-              paddingHorizontal: 5,
-              paddingVertical: 5,
-              backgroundColor: colors.WHITE,
-            }}>
+          <View style={styles.headerContainer}>
             <Pressable
-              style={{
-                justifyContent: 'center',
-                paddingHorizontal: 8,
-                borderWidth: 1,
-                borderColor: colors.GRAY_200,
-                borderRadius: 5,
-              }}
+              style={styles.drawerIconContainer}
               onPress={() => navigation.openDrawer()}>
               <Ionicons name={'md-menu-sharp'} color={colors.BLACK} size={25} />
             </Pressable>
-
-            <Animated.View style={{flex: 1}} entering={SlideInRight}>
+            <Animated.View
+              style={styles.inputContainer}
+              entering={FadeInRight.duration(500)}>
               <SearchInput
                 autoFocus
                 placeholder="주소 또는 제목으로 검색"
                 value={keyword}
                 onChangeText={handleChangeKeyword}
-                onSubmit={() => {}}
+                onSubmit={() => Keyboard.dismiss()}
               />
             </Animated.View>
           </View>
@@ -99,6 +87,24 @@ function FeedSearchItemList({}: FeedSearchItemListProps) {
 const styles = StyleSheet.create({
   contentContainer: {
     paddingHorizontal: 15,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    gap: 5,
+    backgroundColor: colors.WHITE,
+    paddingHorizontal: 5,
+    paddingTop: 5,
+    paddingBottom: 10,
+  },
+  drawerIconContainer: {
+    justifyContent: 'center',
+    paddingHorizontal: 8,
+    borderWidth: 1,
+    borderColor: colors.GRAY_200,
+    borderRadius: 5,
+  },
+  inputContainer: {
+    flex: 1,
   },
 });
 
