2d6171f963225f1fd46ada53fb93cfecfad49293
InKyoJeong <viapolar@naver.com>
2023-05-18 03:12:34 +0900

    feat(front): add setting home screen

--- mylog/src/screens/setting/SettingHomeScreen.tsx
+++ mylog/src/screens/setting/SettingHomeScreen.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  Pressable,
-  SafeAreaView,
-  ScrollView,
-} from 'react-native';
+import {StyleSheet, SafeAreaView, ScrollView, View} from 'react-native';
 import type {DrawerScreenProps} from '@react-navigation/drawer';
 import type {CompositeScreenProps} from '@react-navigation/native';
 import type {StackScreenProps} from '@react-navigation/stack';
+import Octicons from 'react-native-vector-icons/Octicons';
 
 import type {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
 import type {SettingStackParamList} from '@/navigations/stack/SettingStackNavigator';
+import SettingItem from '@/components/setting/SettingItem';
 import {settingNavigations} from '@/constants/navigations';
+import {colors} from '@/constants/colors';
+import useAuth from '@/hooks/queries/useAuth';
 
 type SettingHomeScreenProps = CompositeScreenProps<
   StackScreenProps<
@@ -23,13 +21,37 @@ type SettingHomeScreenProps = CompositeScreenProps<
 >;
 
 function SettingHomeScreen({navigation}: SettingHomeScreenProps) {
+  const {logoutMutation} = useAuth();
+
+  const handlePressEditProfile = () => {
+    navigation.navigate(settingNavigations.EDIT_PROFILE);
+  };
+
+  const handlePressFeedback = () => {
+    navigation.navigate(settingNavigations.FEED_BACK);
+  };
+
+  const handlePressLogout = () => {
+    logoutMutation.mutate(null);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <Pressable
-          onPress={() => navigation.navigate(settingNavigations.EDIT_PROFILE)}>
-          <Text style={{fontSize: 20}}>프로필 수정</Text>
-        </Pressable>
+        <View style={styles.space} />
+        <SettingItem title="프로필 수정" onPress={handlePressEditProfile} />
+        <SettingItem title="다크 모드" />
+        <View style={styles.space} />
+        <SettingItem title="FAQ" />
+        <SettingItem title="의견 보내기" onPress={handlePressFeedback} />
+        <SettingItem title="버전정보" subTitle="0.1.0" />
+        <View style={styles.space} />
+        <SettingItem
+          title="로그아웃"
+          icon={<Octicons name={'sign-out'} color={colors.RED_500} size={16} />}
+          color={colors.RED_500}
+          onPress={handlePressLogout}
+        />
       </ScrollView>
     </SafeAreaView>
   );
@@ -38,6 +60,9 @@ function SettingHomeScreen({navigation}: SettingHomeScreenProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  space: {
+    height: 30,
   },
 });
 
