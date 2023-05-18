2678f2a5eabf62a17c97514331a56c9befd6cc6e
InKyoJeong <viapolar@naver.com>
2023-05-19 00:25:55 +0900

    feat(back): add create feedback dto

--- server/src/feedback/feedback.controller.ts
+++ server/src/feedback/feedback.controller.ts
@@ -8,9 +8,21 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FeedbackService } from './feedback.service';
+import { User } from 'src/auth/user.entity';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { CreateFeedbackDto } from './dto/create-feedback.dto';
 
 @Controller('feedback')
 @UseGuards(AuthGuard())
 export class FeedbackController {
   constructor(private feedbackService: FeedbackService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createFeedback(
+    @Body() createFeedbackDto: CreateFeedbackDto,
+    @GetUser() user: User,
+  ) {
+    return this.feedbackService.createFeedback(createFeedbackDto, user);
+  }
 }
