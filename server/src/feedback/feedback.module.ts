f400f772bea1455e14a62908cf9900f2e81094be
InKyoJeong <viapolar@naver.com>
2023-05-19 00:33:09 +0900

    feat(back): add create feedback service

--- server/src/feedback/feedback.module.ts
+++ server/src/feedback/feedback.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { FeedbackController } from './feedback.controller';
 import { FeedbackService } from './feedback.service';
+import { Feedback } from './feedback.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Feedback]), AuthModule],
   controllers: [FeedbackController],
-  providers: [FeedbackService]
+  providers: [FeedbackService],
 })
 export class FeedbackModule {}
--- server/src/feedback/feedback.service.ts
+++ server/src/feedback/feedback.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Feedback } from './feedback.entity';
+import { CreateFeedbackDto } from './dto/create-feedback.dto';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
-export class FeedbackService {}
+export class FeedbackService {
+  constructor(
+    @InjectRepository(Feedback)
+    private feedbackRepository: Repository<Feedback>,
+  ) {}
+
+  async createFeedback(createFeedbackDto: CreateFeedbackDto, user: User) {
+    const { email, title, description } = createFeedbackDto;
+
+    const feedback = this.feedbackRepository.create({
+      email,
+      title,
+      description,
+      user,
+    });
+
+    await this.feedbackRepository.save(feedback);
+  }
+}
