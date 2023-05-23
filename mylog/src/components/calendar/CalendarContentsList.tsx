7a824a23895f1058ac02b5c9c95d3881e5a4e9c5
InKyoJeong <viapolar@naver.com>
2023-05-23 19:31:48 +0900

    feat(front): add calendar contents list style

--- mylog/src/components/calendar/CalendarContentsList.tsx
+++ mylog/src/components/calendar/CalendarContentsList.tsx
@@ -1,26 +1,38 @@
-import {CalendarPost} from '@/api';
-import {colors} from '@/constants';
-import useThemeStore from '@/store/useThemeStore';
-import {ThemeMode} from '@/types';
 import React from 'react';
 import {Text} from 'react-native';
 import {ScrollView, StyleSheet, View} from 'react-native';
 
-interface CalendarDetailListProps {
+import useThemeStore from '@/store/useThemeStore';
+import {colors} from '@/constants';
+import type {CalendarPost} from '@/api';
+import type {ThemeMode} from '@/types';
+
+interface CalendarContentsListProps {
   posts: CalendarPost[];
 }
 
-function CalendarDetailList({posts}: CalendarDetailListProps) {
+function CalendarContentsList({posts}: CalendarContentsListProps) {
   const {theme} = useThemeStore();
   const styles = styling(theme);
 
   return (
     <ScrollView style={styles.container}>
-      {posts?.map(post => (
-        <View key={post.id}>
-          <Text style={styles.text}>{post.title}</Text>
-        </View>
-      ))}
+      <View style={styles.innerContainer}>
+        {posts?.map(post => (
+          <View key={post.id} style={styles.itemContainer}>
+            <View style={styles.itemHeader} />
+            <View style={styles.infoContainer}>
+              <Text
+                style={styles.addressText}
+                numberOfLines={1}
+                ellipsizeMode="tail">
+                {post.address}
+              </Text>
+              <Text style={styles.titleText}>{post.title}</Text>
+            </View>
+          </View>
+        ))}
+      </View>
     </ScrollView>
   );
 }
@@ -29,10 +41,34 @@ const styling = (theme: ThemeMode) =>
   StyleSheet.create({
     container: {
       backgroundColor: colors[theme].WHITE,
+      padding: 20,
     },
-    text: {
+    innerContainer: {
+      gap: 20,
+      marginBottom: 50,
+    },
+    itemContainer: {
+      flexDirection: 'row',
+    },
+    itemHeader: {
+      backgroundColor: colors[theme].PINK_700,
+      width: 8,
+      height: 50,
+      marginRight: 8,
+      borderRadius: 20,
+    },
+    infoContainer: {
+      justifyContent: 'space-evenly',
+    },
+    addressText: {
+      color: colors[theme].GRAY_500,
+      fontSize: 13,
+    },
+    titleText: {
       color: colors[theme].BLACK,
+      fontSize: 18,
+      fontWeight: '600',
     },
   });
 
-export default CalendarDetailList;
+export default CalendarContentsList;
