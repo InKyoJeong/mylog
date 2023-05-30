f13624ad67ca9144a7fe48c5ae276e8d48a1214f
InKyoJeong <viapolar@naver.com>
2023-05-30 23:16:42 +0900

    feat(back): setup build

---
+++ server/dist/feedback/feedback.service.d.ts
@@ -0,0 +1,10 @@
+import { Repository } from 'typeorm';
+import { Feedback } from './feedback.entity';
+import { CreateFeedbackDto } from './dto/create-feedback.dto';
+import { User } from 'src/auth/user.entity';
+export declare class FeedbackService {
+    private feedbackRepository;
+    constructor(feedbackRepository: Repository<Feedback>);
+    getFeedbacks(): Promise<Feedback[]>;
+    createFeedback(createFeedbackDto: CreateFeedbackDto, user: User): Promise<void>;
+}
