4512b04dc18c4f0595c5bd6ebf760cb1d33d1100
InKyoJeong <viapolar@naver.com>
2023-06-02 17:31:59 +0900

    feat(back): upload image to s3

--- server/src/image/image.controller.ts
+++ server/src/image/image.controller.ts
@@ -6,40 +6,50 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import * as fs from 'fs';
-import { diskStorage } from 'multer';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { extname, basename } from 'path';
-
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { numbers } from 'src/constants';
-
-try {
-  fs.readdirSync('uploads');
-} catch (error) {
-  console.error('create uploads folder');
-  fs.mkdirSync('uploads');
-}
 
 @Controller('images')
 @UseGuards(AuthGuard())
 export class ImageController {
   @UseInterceptors(
     FilesInterceptor('images', numbers.MAX_IMAGE_COUNT, {
-      storage: diskStorage({
-        destination(req, file, cb) {
-          cb(null, 'uploads/');
-        },
-        filename(req, file, cb) {
-          const ext = extname(file.originalname);
-          cb(null, basename(file.originalname, ext) + Date.now() + ext);
-        },
-      }),
       limits: { fileSize: numbers.MAX_IMAGE_SIZE },
     }),
   )
   @Post('/')
-  uploadImages(@UploadedFiles() files: Express.Multer.File[]) {
-    const uris = files.map((file) => file.filename);
+  async uploadImages(@UploadedFiles() files: Express.Multer.File[]) {
+    const s3Client = new S3Client({
+      region: process.env.AWS_BUCKET_REGION,
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      },
+    });
+
+    const uploadPromises = files.map((file) => {
+      const ext = extname(file.originalname);
+      const fileName = basename(file.originalname, ext) + Date.now() + ext;
+      const uploadParams = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `original/${fileName}`,
+        Body: file.buffer,
+      };
+
+      const command = new PutObjectCommand(uploadParams);
+      return s3Client.send(command);
+    });
+
+    await Promise.all(uploadPromises);
+
+    const uris = files.map((file) => {
+      const ext = extname(file.originalname);
+      const fileName = basename(file.originalname, ext) + Date.now() + ext;
+
+      return `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_BUCKET_REGION}.amazonaws.com/original/${fileName}`;
+    });
 
     return uris;
   }
