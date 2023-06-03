501dc6b255b1c8470ab2dcdc3a231858220acc37
InKyoJeong <viapolar@naver.com>
2023-06-03 22:19:05 +0900

    refactor(front): remove unnecessary code - 불필요한 ref 제거

--- mylog/src/components/calendar/YearSelector.tsx
+++ mylog/src/components/calendar/YearSelector.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Pressable, ScrollView, StyleSheet, Text, View} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
@@ -22,23 +22,18 @@ function YearSelector({
 }: YearSelectorProps) {
   const {theme} = useThemeStore();
   const styles = styling(theme);
-  const scrollViewRef = useRef<ScrollView>(null);
   const [scrollY, setScrollY] = useState(0);
 
   useEffect(() => {
-    if (scrollViewRef.current) {
-      const yearIndex = currentyear - 1980;
-      const currentRow = Math.floor(yearIndex / 4);
-      const scrollToY = currentRow * 50;
-      setScrollY(scrollToY);
-    }
+    const currentRow = Math.floor(currentyear - 1980 / 4);
+    const scrollToY = currentRow * 50;
+    setScrollY(scrollToY);
   }, [isVisible, currentyear]);
 
   return (
     <Conditional condition={isVisible}>
       <View style={styles.container}>
         <ScrollView
-          ref={scrollViewRef}
           style={styles.scrollContainer}
           contentOffset={{x: 0, y: scrollY}}>
           <View style={styles.yearsContainer}>
