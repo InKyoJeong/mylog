60657a77e65982277c61b839903b6ba1a2dcae54
InKyoJeong <viapolar@naver.com>
2023-06-10 12:44:20 +0900

    feat(front): add count usequeries

--- mylog/src/hooks/queries/useGetPostCount.ts
+++ mylog/src/hooks/queries/useGetPostCount.ts
@@ -1,31 +1,45 @@
-import {UseQueryOptions, useQuery} from '@tanstack/react-query';
+import {useQueries} from '@tanstack/react-query';
 
+import useAuth from './useAuth';
 import {ResponseCount, getColorCount, getScoreCount} from '@/api';
-import {queryKeys} from '@/constants';
-import type {MarkerColor, ResponseError} from '@/types';
+import {colorHex, countColor, queryKeys} from '@/constants';
+import type {MarkerColor} from '@/types';
 
-function useGetColorCount(
-  queryOptions?: UseQueryOptions<ResponseCount<MarkerColor>, ResponseError>,
-) {
-  return useQuery<ResponseCount<MarkerColor>, ResponseError>(
-    [queryKeys.POST, queryKeys.GET_COLOR_COUNT],
-    () => getColorCount(),
-    {
-      ...queryOptions,
-    },
-  );
+function useGetPostCount() {
+  const {getProfileQuery} = useAuth();
+  const {categories} = getProfileQuery.data || {};
+
+  return useQueries({
+    queries: [
+      {
+        queryKey: [queryKeys.POST, queryKeys.GET_COLOR_COUNT],
+        queryFn: () => getColorCount(),
+        select: (data: ResponseCount<MarkerColor>) =>
+          data.map((countData, id) => ({
+            id,
+            ...countData,
+            field: categories?.[countData.field] ?? '',
+            color: colorHex()[countData.field],
+          })),
+        enabled: Boolean(categories),
+        suspense: true,
+        useErrorBoundary: true,
+      },
+      {
+        queryKey: [queryKeys.POST, queryKeys.GET_SCORE_COUNT],
+        queryFn: () => getScoreCount(),
+        select: (data: ResponseCount<number>) =>
+          data.map((countData, id) => ({
+            ...countData,
+            id,
+            field: `${countData.field}점`,
+            color: countColor[countData.field],
+          })),
+        suspense: true,
+        useErrorBoundary: true,
+      },
+    ],
+  });
 }
 
-function useGetScoreCount(
-  queryOptions?: UseQueryOptions<ResponseCount<number>, ResponseError>,
-) {
-  return useQuery<ResponseCount<number>, ResponseError>(
-    [queryKeys.POST, queryKeys.GET_SCORE_COUNT],
-    () => getScoreCount(),
-    {
-      ...queryOptions,
-    },
-  );
-}
-
-export {useGetColorCount, useGetScoreCount};
+export default useGetPostCount;
