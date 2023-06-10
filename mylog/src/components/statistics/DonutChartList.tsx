141a143056b3f767e83ef34988e4a1be5451df8b
InKyoJeong <viapolar@naver.com>
2023-06-10 18:46:45 +0900

    fix(front): fix donut chart error - 데이터가 없을때 발생하는 오류 수정

--- mylog/src/components/statistics/DonutChartList.tsx
+++ mylog/src/components/statistics/DonutChartList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
+import {ScrollView, StyleSheet} from 'react-native';
 
 import DonutChart from '@/components/statistics/DonutChart';
 import useGetPostCount from '@/hooks/queries/useGetPostCount';
@@ -8,11 +8,17 @@ function DonutChartList() {
   const [{data: colorCount = []}, {data: scoreCount = []}] = useGetPostCount();
 
   return (
-    <ScrollView>
+    <ScrollView style={styles.container} scrollIndicatorInsets={{right: 1}}>
       <DonutChart data={colorCount} title="카테고리" />
       <DonutChart data={scoreCount} title="점수" />
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+});
 
 export default DonutChartList;
