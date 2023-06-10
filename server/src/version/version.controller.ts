5dce6250d391f5a8059fd76d37d0a5f8835ca00b
InKyoJeong <viapolar@naver.com>
2023-06-10 18:06:56 +0900

    fix(back): add missing guard

--- server/src/version/version.controller.ts
+++ server/src/version/version.controller.ts
@@ -14,6 +14,7 @@ import { VersionService } from './version.service';
 import { UpdateVersionDto } from './dto/update-version.dto';
 
 @Controller('version')
+@UseGuards(AuthGuard())
 export class VersionController {
   constructor(private versionService: VersionService) {}
 
